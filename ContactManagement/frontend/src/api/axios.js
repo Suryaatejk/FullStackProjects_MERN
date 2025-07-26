3f3fb4f17da895de61ae4be49e1dac93985c54ae
Suryaatejk <ksuryateja2004@gmail.com>
2025-07-26 21:23:52 +0530

    Update /api

--- ContactManagement/frontend/src/api/axios.js
+++ ContactManagement/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://contactmanagement-backend.onrender.com',
+    baseURL: 'https://contactmanagement-backend.onrender.com/api',
 });
 
 instance.interceptors.request.use((config)=> {
